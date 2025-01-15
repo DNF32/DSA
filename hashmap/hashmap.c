4a32b1663d5922ac939f6b39b74702c16114d8e5
DNF32 <137726307+DNF32@users.noreply.github.com>
2025-01-15 00:00:20 +0000

    fist implementation of hashmap

--- hashmap/hashmap.c
+++ hashmap/hashmap.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 typedef struct node node_t;
 struct node {
@@ -17,14 +18,14 @@ struct hashmap {
 };
 
 hash_t *init_map(int capacity) {
-  hash_t *map = (hash_t*)malloc(sizeof(hash_t));
+  hash_t *map = (hash_t *)malloc(sizeof(hash_t));
   if (map == NULL) {
     printf("Failed to initialize hashmap insidi init\n");
     return NULL;
   }
-  map->capacity = capacity;
+  map->capacity = 0;
   map->array = malloc(capacity * sizeof(node_t *));
-  if (map->array==NULL) {
+  if (map->array == NULL) {
     printf("Failed to initialize array\n");
     return NULL;
   }
@@ -37,11 +38,11 @@ hash_t *init_map(int capacity) {
 
 int hashfunc(int key, int capacity) { return key % (capacity - 1); }
 
-void insert_first_first(hash_t *map, int key, int value) {
+void insert_first(hash_t *map, int key, int value) {
   int index = hashfunc(key, map->capacity);
 
   node_t *list = map->array[index];
-// if the index is empty
+  // if the index is empty
   if (list == NULL) {
     node_t *head = malloc(sizeof(node_t));
     if (head == NULL) {
@@ -51,6 +52,7 @@ void insert_first_first(hash_t *map, int key, int value) {
     head->value = value;
     head->next = NULL;
 
+    map->load += 1;
     map->array[index] = head;
     return;
   }
@@ -73,11 +75,88 @@ void insert_first_first(hash_t *map, int key, int value) {
   new_head->key = key;
   new_head->value = value;
   new_head->next = list;
+  new_head->prev = NULL;
   //
   list->prev = new_head;
 
+  map->load += 1;
   map->array[index] = new_head;
 }
+
+void delete(hash_t *map, int key) {
+  int index = hashfunc(key, map->capacity);
+
+  node_t *head = map->array[index];
+  // if the index is empty
+  if (head == NULL) {
+    printf("The key is not yet used");
+    return;
+  }
+
+  if (head->key == key) {
+    map->array[index] = head->next;
+    map->load -= 1;
+    free(head);
+    return;
+  }
+  while (head != NULL) {
+    if (head->key == key) {
+      node_t *prev = head->prev;
+
+      // need fo check if head is the final node
+      if (head->next != NULL) {
+        node_t *next = head->next;
+
+        prev->next = next;
+        next->prev = prev;
+      } else {
+        prev->next = NULL;
+      }
+      map->load -= 1;
+      free(head);
+      return;
+    }
+    head = head->next;
+  }
+  printf("The key is not yet used");
+}
+
+int search(hash_t *map, int key) {
+  int index = hashfunc(key, map->capacity);
+  // transverse the tree to see if the key is there
+  node_t *head = map->array[index];
+  while (head != NULL) {
+    if (head->key == key) {
+      return head->value;
+    }
+    head = head->next;
+  }
+  return 0;
+}
+
+// free for doubly linked list 
+void free_list(node_t **head) {
+  node_t *current = *head;
+  while (current != NULL) {
+    node_t *temp = current;
+    current = current->next;
+    free(temp);
+  }
+  *head = NULL;
+}
+
+// free map
+void free_map(hash_t **pmap) {
+  hash_t *map = *pmap;
+  for (int i = 0; i < map->load; i++) {
+    node_t **plist = &(map->array[i]);
+    free_list(plist);
+  }
+  free(map->array);
+  free(map);
+  *pmap = NULL;
+}
+
 void print_map(hash_t *map) {
   for (int i = 0; i < map->capacity; i++) {
     node_t *list = map->array[i];
